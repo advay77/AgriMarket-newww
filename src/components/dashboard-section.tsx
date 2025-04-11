cbfc5271ef2fe7acedc423450adfe281e568c405
Advay Anand <advayanand37@gmail.com>
2025-04-11 11:19:11 +0530

    Update dashboard-section.tsx

--- src/components/dashboard-section.tsx
+++ src/components/dashboard-section.tsx
@@ -38,16 +38,17 @@ const Dashboard: React.FC = () => {
           <div className="absolute inset-0 bg-gradient-to-b from-black/40 via-black/20 to-black/60 backdrop-blur-[2px]"></div>
         </div> */}
 
-        <ImagesSlider images={["https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80%22", "https://images.unsplash.com/photo-1620200423727-8127f75d7f53?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://plus.unsplash.com/premium_photo-1661907005604-cec7ffb6a042?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"]} >
-               <div className=" text-white text-2xl font-bold">
-              <h1 className='text-8xl mb-8 max-w-2xl mx-auto text-white/90 text-shadow-lg animate-description-entry mt-8 '>Welcome To AGRIMARKET</h1>
-                <p className="text-4xl mb-8 max-w-2xl mx-auto text-white/90 text-shadow-lg animate-description-entry mt-8 ">
+      <ImagesSlider images={["https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80%22", "https://images.unsplash.com/photo-1620200423727-8127f75d7f53?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://plus.unsplash.com/premium_photo-1661907005604-cec7ffb6a042?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"]} >
+        <div className="text-white text-2xl font-bold text-center w-full">
+        <h1 className='text-4xl mb-4 max-w-2xl mx-auto text-white/90 text-shadow-lg animate-description-entry mt-4'>Welcome To AGRIMARKET</h1>
+              <p className="text-4xl mb-8 max-w-2xl mx-auto text-white/90 text-shadow-lg animate-description-entry mt-8">
                 Experience farm-fresh produce with cutting-edge agricultural innovation
                 </p>
           
           <SignInButton >
-            <button className="group relative mt-4 ml-28 bg-gradient-to-r from-green-600 to-green-500 text-white font-medium py-4 px-5 rounded-full transform transition-all duration-500 hover:scale-105 hover:shadow-[0_0_30px_rgba(34,197,94,0.3)] animate-button-entry overflow-hidden">
-            <span className="absolute inset-0 bg-gradient-to-r from-green-400 to-green-300 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left"></span>
+          
+  <button className="group relative mt-4 bg-gradient-to-r from-green-600 to-green-500 text-white font-medium py-4 px-5 rounded-full transform transition-all duration-500 hover:scale-105 hover:shadow-[0_0_30px_rgba(34,197,94,0.3)] animate-button-entry overflow-hidden">
+    <span className="absolute inset-0 bg-gradient-to-r from-green-400 to-green-300 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left"></span>
             <span className="">Explore Now</span>
           </button>
           </SignInButton>
